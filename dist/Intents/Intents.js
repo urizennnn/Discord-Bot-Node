b7a5b749d115046833322f67d57952ad6ea09fb6
urizennnnnn <igamerryt@gmail.com>
2023-11-20 00:06:17 +0100

    Delete dist/Intents directory

--- dist/Intents/Intents.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.intents = void 0;
-const discord_js_1 = require("discord.js");
-exports.intents = [
-    discord_js_1.GatewayIntentBits.Guilds,
-    discord_js_1.GatewayIntentBits.GuildMessages,
-    discord_js_1.GatewayIntentBits.MessageContent
-];
