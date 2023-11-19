0d8388cd5a1c48bbdaa66fde04ca2f3de289e021
urizennnn <igamerryt@gmail.com>
2023-11-19 22:03:00 +0100

    help

--- app.ts
+++ app.ts
@@ -11,25 +11,28 @@ const PORT:(string | number) = process.env.PORT || 3000;
 
 app.use(morgan('dev'))
 app.get('/authorize/login',(req:Request,res:Response)=>{
+// res.redirect('http://localhost:5000/auth/discord')
 res.redirect('https://discord-auth-7rvh.onrender.com/auth/discord')
 })
 app.get('/home/dashboard', (req: Request, res: Response) => {
   try {
     const base64String = req.query.user as string; 
+    console.log(base64String)
     if (!base64String) {
       return res.status(400).send('Bad Request: Missing user parameter');
     }
 
     const decodedData = Buffer.from(base64String, 'base64').toString('utf-8');
 
-    console.log(decodedData);
+    console.log(decodedData, 'test');
 
     res.send(decodedData).status(200);
   } catch (error) {
     console.error('Error decoding Base64 string:', error);
-    res.status(500).send('Internal Server Error');
+    res.status(500).send(error);
   }
 });
+
 
 
 app.all('/', (req: Request, res: Response) => {
--- dist/app.js
+++ dist/app.js
@@ -11,12 +11,24 @@ const app = (0, express_1.default)();
 const PORT = process.env.PORT || 3000;
 app.use(morgan('dev'));
 app.get('/authorize/login', (req, res) => {
+    // res.redirect('http://localhost:5000/auth/discord')
     res.redirect('https://discord-auth-7rvh.onrender.com/auth/discord');
 });
 app.get('/home/dashboard', (req, res) => {
-    console.log(req);
-    //@ts-ignore
-    res.send(req.user).status(200);
+    try {
+        const base64String = req.query.user;
+        console.log(base64String);
+        if (!base64String) {
+            return res.status(400).send('Bad Request: Missing user parameter');
+        }
+        const decodedData = Buffer.from(base64String, 'base64').toString('utf-8');
+        console.log(decodedData, 'test');
+        res.send(decodedData).status(200);
+    }
+    catch (error) {
+        console.error('Error decoding Base64 string:', error);
+        res.status(500).send(error);
+    }
 });
 app.all('/', (req, res) => {
     res.send('Working');
